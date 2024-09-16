50856f2ed19e0b426ad97700a19611e83ee27060
Jadoooooou <143825071+Jadoooooou@users.noreply.github.com>
2024-09-15 21:11:02 -0400

    Update Accueil.js

--- src/js/scenes/Accueil.js
+++ src/js/scenes/Accueil.js
@@ -4,6 +4,8 @@ class Accueil extends Phaser.Scene {
       }
     
     preload() {
+        this.load.image("bg","./assets/images/backgrounds/bg_lightened.png");
+        this.load.image("logo","./assets/images/logo.png");
         this.load.image("start","./assets/images/btn_start.png");
         this.load.image("credits","./assets/images/btn_credits.png");
         this.load.image("how","./assets/images/btn_credits.png");
@@ -12,8 +14,14 @@ class Accueil extends Phaser.Scene {
 
     create() {
 
+        let bg = this.add.image(config.width / 2, config.height / 2, "bg")
+        .setScale(2.8);
+
+        let logo = this.add.image(config.width / 2, config.height / 2, "logo")
+        .setScale(1);
+
         const start = this.add.image(config.width - 10, 10, "start")
-        .setPosition(500, 200)
+        .setPosition(500, 450)
         .setScale(5.5)
         .setOrigin(1, 0);
 
@@ -52,6 +60,11 @@ class Accueil extends Phaser.Scene {
         //    this.scene.start("");
         //});   
     }
+
+    update() {
+
+    }
+}
 
     update() {
 
